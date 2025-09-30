e509569e80680d5691875f73f9e7cabd9cc05876
saurav <sauravsingh2205@gmail.com>
2025-10-01 00:18:03 +0530

    update with almost backend

--- src/models/longVideoSchema.js
+++ src/models/longVideoSchema.js
@@ -5,8 +5,11 @@ const longVideoSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String },
     category: { type: String },
-    coverUrls: { type: [String], default: [] }, // multiple images
-    publicId: { type: String, default: () => new mongoose.Types.ObjectId() }, // unique auto-generated
+    coverUrls: [String],
+    price: { type: Number, default: 0 },
+    discount: { type: Number, default: 0 }, // ✅ add discount
+    sizes: [{ type: String }],              // ✅ add sizes
+    publicId: { type: String },             // useful for Cloudinary
   },
   { timestamps: true }
 );
