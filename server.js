b7e95f77873a7a87103a2bedc4be0a6b12ac6c07
saurav <sauravsingh2205@gmail.com>
2026-01-22 14:48:04 +0530

    console to check on server

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use(
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
+console.log("MONGO_URI FROM ENV:", process.env.MONGO_URI);
+
 // MongoDB
 // if (process.env.MONGO_URI) {
 //   mongoose
